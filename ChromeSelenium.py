5a0a6ae862621baf585538cff307b4cd952086ad
Daniel <danielpqb@gmail.com>
2022-03-18 17:15:52 -0300

    excluir

--- ChromeSelenium.py
+++
@@ -1,5 +0,0 @@
-from selenium import webdriver
-
-chrome_browser = webdriver.Chrome('chromedriver.exe')
-
-chrome_browser.get('https://www.google.com/search?q=majestoso&oq=majestoso&aqs=chrome..69i57j0l5.3439j1j7&sourceid=chrome&ie=UTF-8')
